2572c462967b8bcde3782d4583b460d823ebed8f
Liz Steak <lizmaepark@gmail.com>
2018-12-01 21:01:04 -0700

    created firebase function for trips

--- travel-frontend/functions/index.js
+++ travel-frontend/functions/index.js
@@ -1,8 +1,25 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase)
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
 });
+
+const createNotification = (notification) => {
+    return admin.firestore().collection('notifications')
+        .add(notification).then(doc => console.log('notification added', doc))
+}
+
+exports.tripCreated = functions.firestore
+    .document('trips/{tripId}')
+    .onCreate(doc => {
+        const trip = doc.data();
+        const notification = {
+            content: 'Added a new trip',
+            user: `${trip.authorFirstName} ${trip.authorLastName}`,
+            time: admin.firestore.FieldValue.serverTimestamp()
+        }
+        return createNotification(notification)
+    })
+
--- travel-frontend/src/Containers/TripsContainer/TripDetailContainer.jsx
+++ travel-frontend/src/Containers/TripsContainer/TripDetailContainer.jsx
@@ -5,6 +5,30 @@ import { compose } from 'redux';
 import { Redirect } from 'react-router-dom';
 import moment from 'moment';
 
+
+// The behavior for Date objects stored in Firestore is going to change
+// AND YOUR APP MAY BREAK.
+// To hide this warning and ensure your app does not break, you need to add the
+// following code to your app before calling any other Cloud Firestore methods:
+
+//   const firestore = new Firestore();
+//   const settings = {/* your settings... */ timestampsInSnapshots: true};
+//   firestore.settings(settings);
+
+// With this change, timestamps stored in Cloud Firestore will be read back as
+// Firebase Timestamp objects instead of as system Date objects. So you will also
+// need to update code expecting a Date to instead expect a Timestamp. For example:
+
+//   // Old:
+//   const date = snapshot.get('created_at');
+//   // New:
+//   const timestamp = snapshot.get('created_at');
+//   const date = timestamp.toDate();
+
+// Please audit all existing usages of Date when you enable the new behavior. In a
+// future release, the behavior will change to the new behavior, so if you do not
+// follow these steps, YOUR APP MAY BREAK.
+
 const TripDetails = (props) => {
     const { trip, auth } = props;
     if (!auth.uid) return <Redirect to='/login' />
